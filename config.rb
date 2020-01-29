8a29d16c97a44b3f10bc6ff27d05b1325a3bfd28
Ben Sekulowicz-Barclay <ben@beseku.eu>
2020-01-29 17:47:39 +0100

    Edition 2020 (pt1)

--- config.rb
+++ config.rb
@@ -6,11 +6,11 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "work/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.layout = "wrap_post"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
@@ -18,19 +18,13 @@ activate :blog do |blog|
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
 
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
-end
-
-page "/feed.xml", layout: false
-
-configure :development do
-  activate :livereload
 end
 
 configure :build do
---
+++ source/2020-01-01-eurostar-pricing-margins.html.md
@@ -0,0 +1,7 @@
+---
+
+title: Internal tools for managing inventory and pricing at Eurostar
+
+category: project
+
+---
